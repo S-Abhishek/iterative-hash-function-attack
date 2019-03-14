aa7e7066905c592ba7c87a8d2fa28106a09db329
Tamil-maran <tamilmaran.c2016@vitstudent.ac.in>
2019-03-14 19:40:34 +0530

    moved aes to same file

--- md.cpp
+++ md.cpp
@@ -3,18 +3,86 @@
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <cstring>
-#include "utils.h"
 using namespace std;
 
-#define HASH_SIZE 4
+// in bits
+#define HASH_SIZE 32
+#define K 16
+
+void handleErrors(void)
+{
+  ERR_print_errors_fp(stderr);
+  abort();
+}
+
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext)
+{
+  EVP_CIPHER_CTX *ctx;
+
+  int len;
+
+  int ciphertext_len;
+
+  /* Create and initialise the context */
+  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, iv))
+    handleErrors();
+
+  if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+    handleErrors();
+  ciphertext_len = len;
+
+  /* Finalise the encryption. Further ciphertext bytes may be written at
+   * this stage.
+   */
+  // if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+  // ciphertext_len += len;
+
+  /* Clean up */
+  EVP_CIPHER_CTX_free(ctx);
+
+  return ciphertext_len;
+}
+
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+  unsigned char *iv, unsigned char *plaintext)
+{
+  EVP_CIPHER_CTX *ctx;
+
+  int len;
+
+  int plaintext_len;
+
+  /* Create and initialise the context */
+  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+    handleErrors();
+
+  if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+    handleErrors();
+  plaintext_len = len;
+
+  /* Finalise the decryption. Further plaintext bytes may be written at
+   * this stage.
+   */
+  if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) handleErrors();
+  plaintext_len += len;
+
+  /* Clean up */
+  EVP_CIPHER_CTX_free(ctx);
+
+  return plaintext_len;
+}
 
 // Pad given plain text of length l to k bytes
 unsigned char* pad(unsigned char* plain_text, int l, int k){
 
+  int i;
   int pad_len = k - l%k;
   unsigned char* newplain = new unsigned char[l + pad_len]();
-  int i;
-  
+
   copy(plain_text, plain_text + l, newplain);
   
   for( i = l; i < l + pad_len; i++ )
@@ -31,16 +99,28 @@ unsigned char* iterative_hash(unsigned char *plain_text, unsigned char *initial_
     int no_of_blocks = length_message/16;
 
     unsigned char* iv = new unsigned char[16]();
-    unsigned char* h0 = new unsigned char[32]();
-    unsigned char* h1 = new unsigned char[32]();
-    unsigned char* mblock = new unsigned char[16]();
+    unsigned char* h0 = new unsigned char[HASH_SIZE/8]();
+    unsigned char* h1 = new unsigned char[HASH_SIZE/8]();
 
-    copy(initial_hash, initial_hash + 32, h0);
+    // Buffers to hold the padded input and obtained output
+    unsigned char* h0_temp = new unsigned char[16]();
+    unsigned char* h1_temp = new unsigned char[16]();
+
+    unsigned char* mblock = new unsigned char[16]();
+    
+    copy(initial_hash, initial_hash + 4, h0);
     for(i = 0 ; i < no_of_blocks ; i++)
     {
         copy(plain_text + i*16, plain_text + (i+1)*16, mblock);
-        encrypt(mblock, strlen((char *)mblock), h0, iv, h1 );
-        copy(h1, h1 + 32, h0);
+
+        // Padding the input to 128 bits
+        h0_temp = pad(h0, 4, 16);
+
+        // Encryption with the padded values
+        encrypt(mblock, strlen((char *)mblock), h0_temp, iv, h1_temp );
+
+        // Dropping bits except the 1st 32
+        copy(h1_temp, h1_temp + 4, h0);
     }
 
     delete iv;
@@ -53,9 +133,9 @@ unsigned char* iterative_hash(unsigned char *plain_text, unsigned char *initial_
 int main (void)
 {
   unsigned char* plaintext = (unsigned char *)"123458457845847577676645678";
-  unsigned char* initial_hash = (unsigned char *)"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  unsigned char* initial_hash = (unsigned char *)"aaaa";
   
   plaintext = pad(plaintext, strlen ((char *)plaintext), 16);
-  BIO_dump_fp (stdout, (const char *)iterative_hash(plaintext, initial_hash), 32);
+  BIO_dump_fp (stdout, (const char *)iterative_hash(plaintext, initial_hash), 4);
 
 }
--- utils.h
+++
@@ -1,5 +0,0 @@
-int encrypt(unsigned char *plaintext,int plaintext_len, unsigned char *key, 
-  unsigned char *iv, unsigned char *ciphertext);
-
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-  unsigned char *iv, unsigned char *plaintext);
