6ecb2b77d1268cef6862e11f20726fbbb2417a1c
TamilMaran <30867933+Tamil-maran@users.noreply.github.com>
2019-03-14 18:16:23 +0530

    removed string functions

--- md.cpp
+++ md.cpp
@@ -2,7 +2,6 @@
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
-#include <string.h>
 #include <cstring>
 #include "utils.h"
 using namespace std;
@@ -12,11 +11,13 @@ unsigned char* pad(unsigned char* plain_text, int l, int k){
 
   int pad_len = k - l%k;
   unsigned char* newplain = new unsigned char[l + pad_len]();
-  copy(plain_text, plain_text + l, newplain);
   int i;
+  
+  copy(plain_text, plain_text + l, newplain);
+  
   for( i = l; i < l + pad_len; i++ )
     newplain[i] = (unsigned char)pad_len;
-
+  
   return newplain;
 }
 
@@ -26,20 +27,24 @@ unsigned char* iterative_hash(unsigned char *plain_text, unsigned char *initial_
     int i;
     int length_message = strlen((char *)plain_text); 
     int no_of_blocks = length_message/16;
+
     unsigned char* iv = new unsigned char[16]();
-
     unsigned char* h0 = new unsigned char[32]();
-    copy(initial_hash, initial_hash + 32, h0);
-
     unsigned char* h1 = new unsigned char[32]();
+    unsigned char* mblock = new unsigned char[16]();
 
+    copy(initial_hash, initial_hash + 32, h0);
     for(i = 0 ; i < no_of_blocks ; i++)
     {
-        string message_block(plain_text+i*16, plain_text+(i+1)*(16));
-        encrypt((unsigned char*)message_block.c_str(), strlen((char *)message_block.c_str()), h0, iv, h1 );
+        copy(plain_text + i*16, plain_text + (i+1)*16, mblock);
+        encrypt(mblock, strlen((char *)mblock), h0, iv, h1 );
         copy(h1, h1 + 32, h0);
     }
 
+    delete iv;
+    delete h1;
+    delete mblock;
+    
     return h0;
 }
 
@@ -50,4 +55,5 @@ int main (void)
   
   plaintext = pad(plaintext, strlen ((char *)plaintext), 16);
   BIO_dump_fp (stdout, (const char *)iterative_hash(plaintext, initial_hash), 32);
+
 }
